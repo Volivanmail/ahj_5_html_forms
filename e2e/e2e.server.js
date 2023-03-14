e9f40b80674d969c9a28b02b7fb599d75a255b68
Your Name <you@example.com>
2023-03-14 19:19:36 +0300

    gg

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -1,13 +1,13 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('../webpack.prod');
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const config = require("../webpack.prod");
 
 const server = new WebpackDevServer(webpack(config), {});
-server.listen(8080, 'localhost', (err) => {
+server.listen(8080, "localhost", (err) => {
   if (err) {
     return;
   }
   if (process.send) {
-    process.send('ok');
+    process.send("ok");
   }
 });
